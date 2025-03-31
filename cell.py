bd290918782f426d8c9387f5ee5d9c79385adfcd
Archana38980609 <archi.dtg@gmail.com>
2025-03-31 09:24:14 +0000

    Add Cell class with walls, drawing, and path visualization
    - Created a Cell class that keeps track of its four walls.
    - Added x/y coordinates to know where the cell is on the canvas.
    - Made a draw() method to display the cell based on its walls.
    - Added draw_move() method to draw a path between two cells.
    - Uses red for moving forward and gray for backtracking.
    - Updated the main script to create and test different cells.
    - Checked that everything draws correctly, including paths between cells.

---
+++ cell.py
@@ -0,0 +1,46 @@
+from maze import Line, Point
+
+class Cell:
+    def __init__(self,x1,y1,x2,y2,win):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = win
+
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+    def draw(self):
+        if self.has_left_wall:
+            left_wall = Line(Point(self._x1,self._y1),Point(self._x1,self._y2))
+            left_wall.draw(self._win)
+
+
+        if self.has_top_wall:
+            top_wall = Line(Point(self._x1,self._y1),Point(self._x2,self._y1))
+            top_wall.draw(self._win)
+
+
+        if self.has_right_wall:
+            right_wall = Line(Point(self._x2,self._y1),Point(self._x2,self._y2))
+            right_wall.draw(self._win)
+
+
+        if self.has_bottom_wall:
+            bottom_wall = Line(Point(self._x1,self._y2),Point(self._x2,self._y2))
+            bottom_wall.draw(self._win)
+
+    def draw_move(self, to_cell, undo=False):
+        centre_x1 = (self._x1 + self._x2)//2
+        centre_y1 = (self._y1 + self._y2)//2
+
+        centre_x2 = (to_cell._x1 + to_cell._x2)//2
+        centre_y2 = (to_cell._y1 + to_cell._y2)//2
+
+        color = "gray" if undo else "red"
+
+        line = Line(Point(centre_x1, centre_y1), Point(centre_x2, centre_y2))
+        line.draw(self._win, color)
--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from maze import Window,Point,Line
-
+from cell import Cell
 def main():
 
     win = Window(800, 600)
-    p1 = Point(50, 50)
-    p2 = Point(200, 300)
-    line = Line(p1, p2)
-    win.draw_line(line)
+    
+    cell1 = Cell(50, 50, 100, 100, win)
+    cell2.has_left_wall = False
+    cell1.draw()
+
+    cell2 = Cell(150, 50, 200, 100, win)
+    cell2.has_top_wall = False
+    cell2.draw()
+
+    cell3 = Cell(350, 50, 400, 100, win)
+    cell3.has_right_wall = False
+    cell3.draw()
+
+    cell4 = Cell(350, 50, 400, 100, win)
+    cell4.has_bottom_wall = False
+    cell4.draw()
+    
+
     win.wait_for_close()
 
 main()
